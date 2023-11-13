d85ddadbfefc4847ce510aab64f1c7b4797bcb93
KayKayM14 <kayleighkeciamartin@gmail.com>
2023-11-13 05:12:46 -0800

    files

--- main.h
+++ main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include "main.h"
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_string(char *);
+int print_decimal(int value)l
+long long int power(int value);
+long long convertToDecimal(int number, int base);
+int print_binary(int number);
+int print_unsigned(unsigned int number);
 
 #endif
