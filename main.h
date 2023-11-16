fce9b89ae40c11074ceefd013149c206f21f8874
KayKayM14 <kayleighkeciamartin@gmail.com>
2023-11-16 05:44:35 -0800

    prototypes

--- main.h
+++ main.h
@@ -9,5 +9,7 @@ int printf_char(va_list args, int charPrinted);
 int printf_string(va_list args, int charPrinted);
 int printf_percent(int charPrinted);
 int processSpecifier(const char *format, va_list args, int charPrinted);
+void handle_d(const char *format, va_list args);
+void handle_i(const char *format, va_list args);
 
 #endif
